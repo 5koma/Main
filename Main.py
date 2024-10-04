e37fd04ee90ec2110d0e16c2df227c4c9c68e446
5koma <bs221033@sun.ac.jp>
2024-10-04 17:41:43 +0900

    Add files via upload

--- Main.py
+++ Main.py
@@ -43,3 +43,8 @@ def handle_message(event):
 if __name__ == "__main__":
     app.run()
 
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
